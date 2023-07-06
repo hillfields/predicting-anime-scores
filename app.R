325cb825b86ee3bfbfc2b036312e05097447386e
Justin Liu <88809356+hillfields@users.noreply.github.com>
2023-07-05 19:32:03 -0700

    working app

--- app.R
+++ app.R
@@ -4,24 +4,22 @@ library(tidyverse)
 library(tidymodels)
 
 # load data
-load("data/anime_data.RData")
-load("data/genres_mean_scores.RData")
-load("data/themes_mean_scores.Rdata")
-load("data/studios_mean_scores.Rdata")
-load("data/demographics_mean_scores.Rdata")
-load("data/producers_mean_scores.Rdata")
+anime_clean <- read_csv("data/anime_clean.csv")
+mean_scores_genres <- read_csv("data/mean_scores_genres.csv")
+mean_scores_themes <- read_csv("data/mean_scores_themes.csv")
+mean_scores_studios <- read_csv("data/mean_scores_studios.csv")
+mean_scores_demographics <- read_csv("data/mean_scores_demographics.csv")
+mean_scores_producers <- read_csv("data/mean_scores_producers.csv")
 
 # load models
 load("models/lm_fit.rda")
-load("models/boost_fit.rda")
 load("models/knn_fit.rda")
-load("models/pca_boost_fit.rda")
-load("models/pca_lm_fit.rda")
+load("models/boost_fit.rda")
 load("models/rf_fit.rda")
 
 # calculate mean score over all anime in the data
 # used in the category_mean_score() function below
-total_mean_score <- anime_data %>%
+total_mean_score <- anime_clean %>%
   pull(score) %>% 
   mean()
 
@@ -57,31 +55,74 @@ ui <- fluidPage(
   sidebarLayout(
     # inputs on the left
     sidebarPanel(
-      textInput("name", "Name of Anime", "Spy x Family"),
-      selectInput("type", "Type", choices = levels(anime_data$type), selected = "tv"),
-      numericInput("episodes", "Number of Episodes", min = 1, step = 0.5, value = 12),
-      selectInput("source", "Source", choices = levels(anime_data$source), selected = "manga"),
-      numericInput("members", "Number of Members", min = 0, step = 0.5, value = 1202853),
-      numericInput("favorites", "Number of Favorites", min = 0, step = 0.5, value = 36217),
-      selectInput("rating", "Rating", choices = levels(anime_data$rating), selected = "pg_13"),
-      selectizeInput("genres", "Genres", choices = levels(factor(genres_mean_scores$genres)), 
-                     multiple = TRUE, selected = c("Action", "Comedy")),
-      selectizeInput("themes", "Themes", choices = levels(factor(themes_mean_scores$themes)), 
-                     multiple = TRUE, selected = c("Childcare")),
-      selectizeInput("studios", "Studios", choices = levels(factor(studios_mean_scores$studios)), 
-                     multiple = TRUE, selected = c("Wit Studio", "CloverWorks")),
-      selectizeInput("demographics", "Demographics", choices = levels(factor(demographics_mean_scores$demographics)), 
-                     multiple = TRUE, selected = c("Shounen")),
-      selectizeInput("producers", "Producers", choices = NULL, multiple = TRUE)
+      textInput("name", 
+                "Name of Anime", 
+                "Spy x Family"),
+      
+      selectInput("type", 
+                  "Type", 
+                  choices = levels(factor(anime_clean$type)), 
+                  selected = "tv"),
+      
+      selectInput("source", 
+                  "Source", 
+                  choices = levels(factor(anime_clean$source)), 
+                  selected = "manga"),
+      
+      numericInput("members", 
+                   "Number of Members", 
+                   min = 0, 
+                   step = 0.5, 
+                   value = 1361732),
+      
+      numericInput("favorites", 
+                   "Number of Favorites", 
+                   min = 0, 
+                   step = 0.5, 
+                   value = 37682),
+      
+      selectInput("rating", 
+                  "Rating", 
+                  choices = levels(factor(anime_clean$rating)),
+                  selected = "pg_13"),
+      
+      selectizeInput("genres", 
+                     "Genres", 
+                     choices = levels(factor(mean_scores_genres$cleaned_category)), 
+                     multiple = TRUE, 
+                     selected = c("Action", "Comedy")),
+      
+      selectizeInput("themes", 
+                     "Themes", 
+                     choices = levels(factor(mean_scores_themes$cleaned_category)), 
+                     multiple = TRUE, 
+                     selected = c("Childcare")),
+      
+      selectizeInput("studios", 
+                     "Studios", 
+                     choices = levels(factor(mean_scores_studios$cleaned_category)), 
+                     multiple = TRUE, 
+                     selected = c("Wit Studio", "CloverWorks")),
+      
+      selectizeInput("demographics", 
+                     "Demographics", 
+                     choices = levels(factor(mean_scores_demographics$cleaned_category)), 
+                     multiple = TRUE, 
+                     selected = c("Shounen")),
+      
+      selectizeInput("producers", 
+                     "Producers", 
+                     choices = NULL, 
+                     multiple = TRUE)
     ),
     
     # outputs on the right
     mainPanel(
       textOutput("message"),
       textOutput("lm_pred"),
+      textOutput("knn_pred"),
       textOutput("boost_pred"),
-      textOutput("pca_lm_pred"),
-      textOutput("pca_boost_pred")
+      textOutput("rf_pred")
     )
   )
 )
@@ -92,49 +133,33 @@ server <- function(input, output, session) {
   output$message <- renderText(paste0(input$name, " has the following predicted scores:"))
   
   # this is in the server side since there are 1000+ producers
-  updateSelectizeInput(session, "producers", choices = levels(factor(producers_mean_scores$producers)), server = TRUE,
+  updateSelectizeInput(session, 
+                       "producers",
+                       choices = levels(factor(mean_scores_producers$cleaned_category)), 
+                       server = TRUE,
                        selected = c("TV Tokyo", "Shogakukan-Shueisha Productions", "TOHO animation", "Shueisha"))
   
   # turn inputs into reactive variables so that the outputs are updated when the inputs are changed
+  title <- reactive(input$name)
   type <- reactive(input$type)
-  episodes <- reactive(input$episodes)
   anime_source <- reactive(input$source)
   members <- reactive(input$members)
   favorites <- reactive(input$favorites)
   rating <- reactive(input$rating)
-  genres_score <- reactive(category_mean_score(input$genres, genres_mean_scores))
-  themes_score <- reactive(category_mean_score(input$themes, themes_mean_scores))
-  studios_score <- reactive(category_mean_score(input$studios, studios_mean_scores))
-  demographics_score <- reactive(category_mean_score(input$demographics, demographics_mean_scores))
-  producers_score <- reactive(category_mean_score(input$producers, producers_mean_scores))
+  genres_score <- reactive(category_mean_score(input$genres, mean_scores_genres))
+  themes_score <- reactive(category_mean_score(input$themes, mean_scores_themes))
+  studios_score <- reactive(category_mean_score(input$studios, mean_scores_studios))
+  demographics_score <- reactive(category_mean_score(input$demographics, mean_scores_demographics))
+  producers_score <- reactive(category_mean_score(input$producers, mean_scores_producers))
   
   # make predictions
   output$lm_pred <- renderText({
     paste0("> Linear Model: ",
            predict(
-             lm_fit,
+             lm_final_fit,
              new_data = tibble(
+               title = title(),
                type = type(),
-               episodes = episodes(),
-               source = anime_source(),
-               members = members(),
-               favorites = favorites(),
-               rating = rating(),
-               genres_score = genres_score(),
-               themes_score = themes_score(),
-               studios_score = studios_score(),
-               demographics_score = demographics_score(),
-               producers_score = producers_score()
-             ))[[1, 1]] %>% round(2))
-  })
-  
-  output$boost_pred <- renderText({
-    paste0("> Boosted Model: ",
-           predict(
-             final_boost_fit,
-             new_data = tibble(
-               type = type(),
-               episodes = episodes(),
                source = anime_source(),
                members = members(),
                favorites = favorites(),
@@ -147,13 +172,13 @@ server <- function(input, output, session) {
              ))[[1, 1]] %>% round(2))
   })
 
-  output$pca_lm_pred <- renderText({
-    paste0("> PCA Linear Model: ",
+  output$knn_pred <- renderText({
+    paste0("> KNN Model: ",
            predict(
-             pca_final_fit,
+             knn_final_fit,
              new_data = tibble(
+               title = title(),
                type = type(),
-               episodes = episodes(),
                source = anime_source(),
                members = members(),
                favorites = favorites(),
@@ -165,14 +190,14 @@ server <- function(input, output, session) {
                producers_score = producers_score()
              ))[[1, 1]] %>% round(2))
   })
-  
-  output$pca_boost_pred <- renderText({
-    paste0("> PCA Boosted Model: ",
+
+  output$boost_pred <- renderText({
+    paste0("> Boosted Trees Model: ",
            predict(
-             final_pca_boost_fit,
+             boost_final_fit,
              new_data = tibble(
+               title = title(),
                type = type(),
-               episodes = episodes(),
                source = anime_source(),
                members = members(),
                favorites = favorites(),
@@ -188,10 +213,10 @@ server <- function(input, output, session) {
   output$rf_pred <- renderText({
     paste0("> Random Forest Model: ",
            predict(
-             knn_final_fit,
+             boost_final_fit,
              new_data = tibble(
+               title = title(),
                type = type(),
-               episodes = episodes(),
                source = anime_source(),
                members = members(),
                favorites = favorites(),
---
+++ scrape.R
@@ -0,0 +1,172 @@
+# load libraries
+library(tidyverse)
+library(rvest)
+library(xml2)
+
+# function to get anime details from MAL
+get_anime_details <- function(url) {
+  # read the HTML page of the URL for the other missing fields
+  html <- read_html(url)
+  
+  ##### TITLE #####
+  
+  # get English title first
+  title <- html %>%
+    html_nodes(".title-english.title-inherit") %>%
+    html_text2()
+  
+  # if missing, get Japanese title
+  if (identical(title, character(0))) {
+    title <- html %>%
+      html_nodes(".title-name") %>%
+      html_text2()
+  }
+  
+  ##### TYPE #####
+  
+  type <- html %>%
+    html_nodes(".information.type") %>%
+    html_text2() %>%
+    str_to_lower()
+  
+  ##### SOURCE #####
+  
+  source <- html %>%
+    html_nodes(xpath = "..//span[contains(text(), 'Source:')]") %>%
+    html_nodes(xpath = "..") %>%
+    html_text2() %>%
+    str_split(": ") %>%
+    pluck(1) %>%
+    nth(2) %>%
+    str_to_lower() %>%
+    str_replace_all(" |-", "_")
+  
+  ##### MEMBERS #####
+  
+  members <- html %>%
+    html_nodes(xpath = "..//span[contains(text(), 'Members:')]") %>%
+    html_nodes(xpath = "..") %>%
+    html_text2() %>%
+    str_split(": ") %>%
+    pluck(1) %>%
+    gsub(",", "", .) %>%
+    nth(2) %>%
+    as.numeric()
+  
+  ##### FAVORITES #####
+  
+  favorites <- html %>%
+    html_nodes(xpath = "..//span[contains(text(), 'Favorites:')]") %>%
+    html_nodes(xpath = "..") %>%
+    html_text2() %>%
+    str_split(": ") %>%
+    pluck(1) %>%
+    gsub(",", "", .) %>%
+    nth(2) %>%
+    as.numeric()
+  
+  ##### RATING #####
+  
+  rating <- html %>%
+    html_nodes(xpath = "..//span[contains(text(), 'Rating:')]") %>%
+    html_nodes(xpath = "..") %>%
+    html_text2() %>%
+    str_split(": ") %>%
+    pluck(1) %>%
+    nth(2) %>%
+    {case_when(. == "G - All Ages" ~ "g",
+               . == "PG - Children" ~ "pg",
+               . == "PG-13 - Teens 13 or older" ~ "pg_13",
+               . == "R - 17+ (violence & profanity)" ~ "r",
+               . == "R+ - Mild Nudity" ~ "r+",
+               . == "Rx - Hentai" ~ "rx")}
+  
+  ##### GENRES #####
+  
+  genres <- html %>%
+    html_nodes(xpath = "..//span[contains(text(), 'Genres:')]") %>%
+    html_nodes(xpath = "..") %>%
+    html_elements("a") %>%
+    html_text2()
+  
+  if (identical(genres, character(0))) {
+    genres <- "Other or NA"
+  }
+  
+  ##### THEMES #####
+  
+  themes <- html %>%
+    html_nodes(xpath = "..//span[contains(text(), 'Themes:')]") %>%
+    html_nodes(xpath = "..") %>%
+    html_elements("a") %>%
+    html_text2()
+  
+  if (identical(themes, character(0))) {
+    themes <- "Other or NA"
+  }
+  
+  ##### STUDIOS #####
+  
+  studios <- html %>%
+    html_nodes(xpath = "..//span[contains(text(), 'Studios:')]") %>%
+    html_nodes(xpath = "..") %>%
+    html_elements("a") %>%
+    html_text2()
+  
+  ##### DEMOGRAPHIC #####
+  
+  demographic <- html %>%
+    html_nodes(xpath = "..//span[contains(text(), 'Demographic:')]") %>%
+    html_nodes(xpath = "..") %>%
+    html_elements("a") %>%
+    html_text2()
+  
+  if (identical(demographic, character(0))) {
+    demographic <- "Other or NA"
+  }
+  
+  ##### PRODUCERS #####
+  
+  producers <- html %>%
+    html_nodes(xpath = "..//span[contains(text(), 'Producers:')]") %>%
+    html_nodes(xpath = "..") %>%
+    html_elements("a") %>%
+    html_text2()
+  
+  if (identical(producers, character(0))) {
+    producers <- "Other or NA"
+  }
+  
+  ##### SCORE #####
+  
+  score <- html %>%
+    html_nodes(".score-label") %>%
+    html_text2() %>%
+    first()
+  
+  # convert to numeric if score is available
+  if (score != "N/A") {
+    score <- as.numeric(score)
+  }
+  
+  ##### EVERYTHING #####
+  
+  list("title" = title,
+       "type" = type,
+       "source" = source,
+       "members" = members,
+       "favorites" = favorites,
+       "rating" = rating,
+       "genres" = genres,
+       "themes" = themes,
+       "studios" = studios,
+       "demographic" = demographic,
+       "producers" = producers,
+       "score" = score) 
+}
+
+# test function
+# url <- "https://myanimelist.net/anime/51179/Mushoku_Tensei_II__Isekai_Ittara_Honki_Dasu"
+# get_anime_details(url)
+
+get_anime_details("https://myanimelist.net/anime/5114/Fullmetal_Alchemist__Brotherhood")
